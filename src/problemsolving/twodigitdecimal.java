88ad284e7d879b8ac140888a7cdb06fe4a0de167
Sultana Anika  Ahmed Nayona <sbd.bi05@waltonbd.com>
2022-10-10 23:32:44 +0600

    updated one code

--- src/problemsolving/twodigitdecimal.java
+++ src/problemsolving/twodigitdecimal.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 public class twodigitdecimal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Decimal numbers ");
 
-		double num1 = sc.nextDouble();
-		double num2 = sc.nextDouble();
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Decimal numbers ");
 
-		String a = String.format("%.02f", num1);
-		String b = String.format("%.02f", num2);
+			double num1 = sc.nextDouble();
+			double num2 = sc.nextDouble();
 
-		System.out.println(a + "\n" + b);
+			String a = String.format("%.02f", num1);
+			String b = String.format("%.02f", num2);
 
-		if (a.compareTo(b) == 0) {
-			System.out.println("The numbers are same.");
-		}
+			System.out.println(a + "\n" + b);
+			if (a.compareTo(b) == 0) {
+				System.out.println("The numbers are same.");
+			}
 
-		else if (a.compareTo(b) == 1) {
-			System.out.println("The numbers are not same.");
+			else if (a.compareTo(b) != 0) {
+				System.out.println("The numbers are not same.");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+
 		}
 
 	}
