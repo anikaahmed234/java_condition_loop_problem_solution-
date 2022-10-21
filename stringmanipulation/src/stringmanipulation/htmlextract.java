811661d3321fe4c675009824598490835d6b5018
Sultana Anika  Ahmed Nayona <sbd.bi05@waltonbd.com>
2022-10-21 21:56:47 +0600

    String code html

--- stringmanipulation/src/stringmanipulation/htmlextract.java
+++ stringmanipulation/src/stringmanipulation/htmlextract.java
@@ -1,13 +1,4 @@
 package stringmanipulation;
-
-public class htmlextract {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
 //1. Extract the transaction ID from the following HTML body
 //
 //Input:
@@ -18,3 +9,25 @@ public class htmlextract {
 //</body>
 //</html>
 //Output: TXN123456
+
+import java.io.*;
+public class htmlextract {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder html = new StringBuilder();
+		FileReader fr = new FileReader("C:\\Users\\47839\\Pictures\\course mat\\github\\task.html");
+		BufferedReader br = new BufferedReader(fr);
+		String val;
+		while ((val = br.readLine()) != null) {
+			html.append(val);
+		}
+		String result = html.toString();
+		String til = result.toString();
+		String brr[] = til.split("Trnx Id is: ");
+		String arr[] = brr[1].split("<");
+		System.out.println(arr[0]);
+
+
+		br.close();
+	}
+}
